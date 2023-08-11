37ed0f4829e3b9ec854c82714920ac405da98dc3
Qussai <mahmoudqussai96@gmail.com>
2023-08-10 17:12:52 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-/*
- *This is the main function
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
  */
 
 int main(void)
