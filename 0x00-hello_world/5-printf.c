b87097c4783c2fc50041b39a7d03f0f0933c52bd
Qussai <mahmoudqussai96@gmail.com>
2023-08-10 17:20:01 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,8 @@
  * Return: Always return 0 to indicate success
  */
 
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
