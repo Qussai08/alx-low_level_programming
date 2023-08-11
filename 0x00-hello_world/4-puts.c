031a891d2afc4c93900e903d36e6fb50ecfc9802
Qussai <mahmoudqussai96@gmail.com>
2023-08-10 17:11:15 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/*
+ *This is the main function
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
