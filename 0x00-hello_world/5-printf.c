16ac2d0476213fb3d0f340f1654983c2cba64d8d
Qussai <mahmoudqussai96@gmail.com>
2023-08-10 17:16:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Calls printf and recieves value returned by printf after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
