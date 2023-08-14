e8a384732a5e6fd32235a52d96697e323b73369c
Qussai <mahmoudqussai96@gmail.com>
2023-08-14 01:08:39 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c++);
+}
+return (0);
+}
