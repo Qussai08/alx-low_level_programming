742da4bd3e69937224cae3fa419bb7ca346f08db
Qussai <mahmoudqussai96@gmail.com>
2023-08-14 01:14:55 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c == 'e' || c == 'q')
+{
+continue;
+}
+putchar(c++);
+}
+putchar('\n');
+return (0);
+}
