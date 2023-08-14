e4982265c74c1b6596e7196ea915013afe7b40dd
Qussai <mahmoudqussai96@gmail.com>
2023-08-14 01:33:23 -0700

    tebahplA

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c--);
+}
+putchar('\n');
+return (0);
+}
