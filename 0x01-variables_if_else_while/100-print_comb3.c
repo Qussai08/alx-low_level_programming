efb6086291dab625c836f721d93300c6125581f8
Qussai <mahmoudqussai96@gmail.com>
2023-08-14 01:57:00 -0700

    2-digit Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i+1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i == 8 && j == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
