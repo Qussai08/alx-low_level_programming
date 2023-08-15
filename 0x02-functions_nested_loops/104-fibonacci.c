50dd78e3565045bb4ad3b95084dd4631ed38d1d4
Mahmoud Qussai <mahmoudqussai96@gmail.com>
2023-08-15 14:46:40 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 int i;
-unsigned long int j = 1, z = 2;
+unsigned long long int j = 1, z = 2;
 printf("%ld, %ld, ", j, z);
 for (i = 0; i < 96; i++)
 {
 z += j;
 j = z - j;
 if (i == 95)
-printf("%lu\n", z);
+printf("%llu\n", z);
 else
-printf("%lu, ", z);
+printf("%llu, ", z);
 }
 
 return (0);
