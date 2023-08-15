d9a99f1c6c252a301ef895351e8fba498b76d6b0
Qussai <mahmoudqussai96@gmail.com>
2023-08-14 23:38:03 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+ * print_alphabet Function
+ * Description: Print Alphabet
+ */
+
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+}
+_putchar('\n');
+}
