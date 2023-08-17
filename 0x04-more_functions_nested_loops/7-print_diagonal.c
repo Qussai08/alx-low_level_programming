5e7cb8fa286a68f1b97df3a53396a5d156ddf15d
Qussai <mahmoudqussai96@gmail.com>
2023-08-17 06:22:24 -0700

    Print Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal on the consol
+ * @n: the length of the diagonal
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i = 1;
+while (n--)
+{
+if (n < 0)
+break;
+while (i--)
+{
+_putchar(' ');
+}
+_putchar(92);
+}
+_putchar('\n');
+}
