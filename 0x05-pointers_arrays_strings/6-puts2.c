effea58c21190c558e552511d7410df8e3a7a3f3
Qussai <mahmoudqussai96@gmail.com>
2023-08-21 04:57:40 -0700

    Puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts2 - prints every other character of a string followed by a new line
+ * puts2 - prints every other character of a string followed by a new line
  * @str: the string to be printed
  *
  * Return: void
