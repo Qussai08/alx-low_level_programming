7670a0cc8a5043e78543d190347cfa0c5bdbbd6f
Qussai <mahmoudqussai96@gmail.com>
2023-08-21 04:12:35 -0700

    Reverse String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 {
 int i = 0;
 char *a;
-a = s;
+*a = *s;
 
 while (s[i] != '\0')
 {
@@ -27,7 +27,7 @@ i--;
 }
 while (s[i] != '\0')
 {
-s[i] = a[i];
+*(s + i) = *(a + i);
 i++;
 }
 }
