732507c9b9ab24a495ff2b0f630ab51d7f4cf371
Qussai <mahmoudqussai96@gmail.com>
2023-08-21 05:05:26 -0700

    Puts Half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,16 +10,16 @@
 void puts_half(char *str)
 {
 int i = 0;
-while (*(s + i) != '\0')
+while (*(str + i) != '\0')
 {
 i++;
 }
 
 i /= 2;
 
-while (s[i] != '\0')
+while (str[i] != '\0')
 {
-_putchar(s[i]);
+_putchar(str[i]);
 i++;
 }
 _putchar('\n');
