25417278e8d82f08800e11f433496dbf4b8e3ec6
Qussai <mahmoudqussai96@gmail.com>
2023-08-23 07:42:39 -0700

    Cap String

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,18 +2,16 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- *
+ * @s: string to be changed
  * Return: char *
  */
 
 char *string_toupper(char *s)
 {
-int i, j = 0;
-while (s[j] != '\0')
-j++;
-
-for (i = 0; i < j; i++)
+int i = 0;
+while (s[i] != '\0')
 {
+i++;
 if (s[i] >= 97 && s[i] <= 122)
 s[i] -= 32;
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string
+ * @s: string to be capitalized
+ *
+ * Return: char *
+ */
+
+char *cap_string(char *s)
+{
+int i, j = 0;
+int cspc = 13;
+char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+while (s[j] != '\0')
+{
+for (i = 0; i < cspc; i++)
+{
+if ((s[j] >= 97 && s[j] <= 122) && s[j - 1] == spc[i])
+s[j] -= 32;	
+}
+j++;
+}
+return (s);
+}
