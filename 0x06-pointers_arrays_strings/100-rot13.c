42d4aa552399c8de21682d6bab0b1091a2b9efa8
Qussai <mahmoudqussai96@gmail.com>
2023-08-24 06:17:45 -0700

    ROT13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - is a simple letter substitution cipher
+ * @s: string to be encrypted
+ *
+ * Return: char *
+ */
+
+char *rot13(char *s)
+{
+int i = 0;
+while (s[i])
+{
+while ((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90))
+{
+if (s[i] > 77 || s[i] > 109)
+{
+s[i] -= 12;
+break;
+}
+s[i] += 13;
+break;
+}
+i++;
+}		
+return (s);
+}
