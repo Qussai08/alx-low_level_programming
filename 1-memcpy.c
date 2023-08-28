815bb157ca014de822fa178c25f2fa7a9409ece0
Qussai <mahmoudqussai96@gmail.com>
2023-08-28 00:26:12 -0700

    Memcpy

---
+++ 1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: the area to store copied memory area
+ * @src: the memory area to be copied
+ * @n: bytes of memory areas to be copied
+ *
+ * Return: char *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i]
+
+return (dest);
+}
