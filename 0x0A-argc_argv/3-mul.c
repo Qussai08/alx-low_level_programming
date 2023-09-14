bb4aed05e15fc50505f392bfa6cc0b4c7d887914
Mahmoud Qussai <mahmoudqussai96@gmail.com>
2023-09-14 09:52:41 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,9 +11,12 @@
 
 int main(int argc, char *argv[])
 {
-if (argc > 2)
+int n1 = 0 , n2 = 0;
+if (argc == 3)
 {
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 * n2);
 }
 else
 {
